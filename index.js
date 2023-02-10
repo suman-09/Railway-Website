591b091b7cfc25e02d383cf9f96af8739c87a65b
Satyam0204 <satyampattanaik007@gmail.com>
2023-02-10 13:16:33 +0530

    changed the functionality for multiple dates

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ document.getElementById('search').addEventListener('click',(e)=>{
     
     var starting =document.getElementById('starting').value
     var destination =document.getElementById('destination').value
+    var searchedDate =document.getElementById('date').value
     
     fetch('https://demorailapi.onrender.com/availability')
     .then((response)=>(response.json()))
@@ -15,8 +16,8 @@ document.getElementById('search').addEventListener('click',(e)=>{
         const trains=data
         console.log(data)
         trains.map((train)=>{
-            if(starting.toLowerCase()===train.info.starting.toLowerCase() && destination.toLowerCase()===train.info.destination.toLowerCase()){
-
+            if(starting.toLowerCase()===train.info.starting.toLowerCase() && destination.toLowerCase()===train.info.destination.toLowerCase() && train.availability.date.includes(searchedDate.toString())){
+                const index=train.availability.date.indexOf(searchedDate)
                 results.innerHTML+=`<div class="card">
                 <div class="info" id="trainNo">${train.trainno}</div>
                 <div class="info" id="trainName">${train.trainname}</div>
@@ -26,12 +27,12 @@ document.getElementById('search').addEventListener('click',(e)=>{
                 </div>
                 <div class="info" id="availibilty">
                 <div class="info" id="berth">
-                    <div>${train.availability.CC?"CC : "+train.availability.CC:""}</div>
-                    <div>${train.availability.S2?"2S : "+train.availability.S2:""}</div>
-                    <div>${train.availability.SL?"SL : "+train.availability.SL:""}</div>
-                    <div>${train.availability.A1?"1A : "+train.availability.A1:""}</div>
-                    <div>${train.availability.A2?"2A : "+train.availability.A2:""}</div>
-                    <div>${train.availability.A3?"3A : "+train.availability.A3:""}</div>
+                    <div>${train.availability.CC[index]?"CC : "+train.availability.CC[index]:""}</div>
+                    <div>${train.availability.S2[index]?"2S : "+train.availability.S2[index]:""}</div>
+                    <div>${train.availability.SL[index]?"SL : "+train.availability.SL[index]:""}</div>
+                    <div>${train.availability.A1[index]?"1A : "+train.availability.A1[index]:""}</div>
+                    <div>${train.availability.A2[index]?"2A : "+train.availability.A2[index]:""}</div>
+                    <div>${train.availability.A3[index]?"3A : "+train.availability.A3[index]:""}</div>
                 </div>
                 <div class="info" id="seatsno"></div>
                 </div>
